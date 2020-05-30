3dd15e0224377b0da1aa5552546a4395feeab812
Greg Lafrance <greglafrance007@gmail.com>
2020-05-30 01:39:43 -0400

    Finished graphics SVG section.

--- course-code/course-project/great-world/assets/code/graphics_code.js
+++ course-code/course-project/great-world/assets/code/graphics_code.js
@@ -1,24 +1,65 @@
 // Function to execute when graphics HTML page is loaded.
 function onLoadGraphics() {
-    // Call function that loads graphics in graphics.html page.
-    loadGraphics();
+    // Set link click handlers.
+    setClickHandlers();
+
+    // Set SVG radio button group change handler.
+    setSVGChangeHandler();
 }
 
-// Programmatically add HTML to display graphics.
-function loadGraphics() {
-    /*
-    // Get array of video paths. 
-    // getVideoList() function is in a different file, but that file is also 
-    // loaded by web page with another <script> tag, so its code is in scope.
-    var videoPaths = getVideoList();
+// Set link click handlers, to show the SVG or Canvas graphics UI.
+function setClickHandlers() {
+    $("#svgLink").click(function () {
+        // "this" is the link that was clicked.
+        setActive(this);
 
-    // Dynamically create HTML for each video, add each to the main content wrapper.
-    videoPaths.forEach(function (path) {
-        // Use jQuery to create the <video> tag and supporting HTML.
-        // var image = $('<img src="' + path + '" class="image-thumbnail"/>');
-
-        // Add the video related elements to the main content wrapper.
-        // $("#main_wrapper").append(image);
+        // Show the proper graphics container.
+        $("#canvasContainer").hide();
+        $("#svgContainer").show();
     });
-    */
+
+    $("#canvasLink").click(function () {
+        // "this" is the link that was clicked.
+        setActive(this);
+
+        // Show the proper graphics container.
+        $("#svgContainer").hide();
+        $("#canvasContainer").show();
+    });
+}
+
+// Set the proper link to be styled as active.
+function setActive(link) {
+    // Clear the active CSS class for both links.
+    $("#graphics_links > div").removeClass("active");
+
+    // Add the active CSS class for the link that was clicked.
+    $(link).addClass("active");
+}
+
+// Respond to SVG radio button clicks, to show the corresponding example.
+function setSVGChangeHandler() {
+    // Now show the correct example, based on the radio button click.
+    $('input[type=radio]').change(function() {
+        // First hide all the examples.
+        $(".example").hide();
+
+        switch (this.id) {
+            case 'rectangle':
+                $("#rectangle_example").show();
+                break;
+            case 'circle':
+                $("#circle_example").show();
+                break;
+            case 'line':
+                $("#line_example").show();
+                break;
+            case 'path':
+                $("#path_example").show();
+                break;
+            case 'text':
+                $("#text_example").show();
+                break;
+        }
+    });
 }
