8cfe9adf3adeb22afbc22a095c9c26e462606894
Greg Lafrance <greglafrance007@gmail.com>
2020-05-31 17:04:07 -0400

    Completed graphics page, added link for fun stuff page, and initially copied quotes page which will contain fun stuff.

--- course-code/course-project/great-world/assets/code/graphics_code.js
+++ course-code/course-project/great-world/assets/code/graphics_code.js
@@ -3,8 +3,12 @@ function onLoadGraphics() {
     // Set link click handlers.
     setClickHandlers();
 
-    // Set SVG radio button group change handler.
+    // Set SVG and canvas radio button group change handler.
     setSVGChangeHandler();
+    setCanvasChangeHandler();
+
+    // Draw the canvas rectangle to initilize it.
+    drawRectangle();
 }
 
 // Set link click handlers, to show the SVG or Canvas graphics UI.
@@ -40,26 +44,177 @@ function setActive(link) {
 // Respond to SVG radio button clicks, to show the corresponding example.
 function setSVGChangeHandler() {
     // Now show the correct example, based on the radio button click.
-    $('input[type=radio]').change(function() {
+    $('#svg-example-links input').change(function() {
         // First hide all the examples.
-        $(".example").hide();
+        $(".svg-example").hide();
 
+        // Now show the appropriate SVG example.
         switch (this.id) {
-            case 'rectangle':
-                $("#rectangle_example").show();
+            case 'svg_rectangle':
+                $("#svg_rectangle_example").show();
                 break;
-            case 'circle':
-                $("#circle_example").show();
+            case 'svg_circle':
+                $("#svg_circle_example").show();
                 break;
-            case 'line':
-                $("#line_example").show();
+            case 'svg_line':
+                $("#svg_line_example").show();
                 break;
-            case 'path':
-                $("#path_example").show();
+            case 'svg_path':
+                $("#svg_path_example").show();
                 break;
-            case 'text':
-                $("#text_example").show();
+            case 'svg_text':
+                $("#svg_text_example").show();
                 break;
         }
     });
 }
+
+// Respond to canvas radio button clicks, to show the corresponding example.
+function setCanvasChangeHandler() {
+    // Show the correct example, based on the radio button click.
+    $('#canvas-example-links input').change(function() {
+        switch (this.id) {
+            case 'canvas_rectangle':
+                drawRectangle();
+                break;
+            case 'canvas_circle':
+                drawCircle();
+                break;
+            case 'canvas_line':
+                drawLine();
+                break;
+            case 'canvas_path':
+                drawPath();
+                break;
+            case 'canvas_text':
+                drawText();
+                break;
+        }
+    });
+}
+
+// Draw a rectangle on the canvas.
+function drawRectangle() {
+    var canvas = document.getElementById("canvasElem");
+    var ctx = canvas.getContext("2d");
+
+    // First clear the canvas.
+    clearCanvas();
+
+    ctx.fillStyle = "green";
+    ctx.strokeStyle = "yellow";
+    ctx.lineWidth = 2;
+    ctx.strokeRect(0, 0, 400, 300);
+    ctx.fillRect(2, 2, 397, 297);
+}
+
+// Draw a circle on the canvas.
+function drawCircle() {
+    var canvas = document.getElementById("canvasElem");
+    var ctx = canvas.getContext("2d");
+
+    // First clear the canvas.
+    clearCanvas();
+
+    var centerX = (canvas.width / 2) - 10;
+    var centerY = (canvas.height / 2) - 50;
+    var radius = 160;
+
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = "purple";
+    ctx.fill();
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "orange";
+    ctx.stroke();
+}
+
+function drawLine() {
+    var canvas = document.getElementById("canvasElem");
+    var ctx = canvas.getContext("2d");
+
+    // First clear the canvas.
+    clearCanvas();
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "blue";
+    ctx.moveTo(20, 50);
+    ctx.lineTo(400, 200);
+    ctx.stroke(); 
+}
+
+function drawPath() {
+    var canvas = document.getElementById("canvasElem");
+    var ctx = canvas.getContext("2d");
+
+    // First clear the canvas.
+    clearCanvas();
+
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "red";
+    ctx.moveTo(225, 50);
+    ctx.lineTo(80, 340);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(225, 50);
+    ctx.lineTo(370, 340);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.moveTo(150, 200);
+    ctx.lineTo(300, 200);
+    ctx.stroke();
+
+    // <path d="M 100 350 q 150 -300 300 0" stroke="blue"
+    // stroke-width="5" fill="none" />
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "blue";
+    ctx.moveTo(75, 350);
+    ctx.bezierCurveTo(210, 91, 300, 220, 371, 341);
+    ctx.stroke();
+
+    ctx.fillStyle = "orange";
+    ctx.beginPath();
+    ctx.arc(225, 50, 8, 0, 2 * Math.PI, false);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(75, 350, 8, 0, 2 * Math.PI, false);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(375, 350, 8, 0, 2 * Math.PI, false);
+    ctx.fill();
+
+    ctx.font = "29px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText("A", 35, 350);
+    ctx.fillText("B", 215, 40);
+    ctx.fillText("C", 395, 350);
+
+}
+
+function drawText() {
+    var canvas = document.getElementById("canvasElem");
+    var ctx = canvas.getContext("2d");
+
+    // First clear the canvas.
+    clearCanvas();
+
+    ctx.font = "50px Arial";
+    ctx.fillStyle = "pink";
+    ctx.fillText("I LOVE CANVAS", 20, 130);
+}
+
+function clearCanvas() {
+    var canvas = document.getElementById("canvasElem");
+    var ctx = canvas.getContext("2d");
+    ctx.lineWidth = 2;
+    ctx.clearRect(0, 0, 500, 500);
+}
+
